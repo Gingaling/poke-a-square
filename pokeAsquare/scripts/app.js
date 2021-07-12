2dc13364e7ae0235427127fca9463ef6da337765
DaltonHart <daltonvaren@gmail.com>
2021-07-12 09:11:44 -0700

    adds user story breakdown

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,6 +1,8 @@
-console.log('Welcome to Poke-A-Square...');
+console.log("Welcome to Poke-A-Square...");
 
 // == User Stories/game logic == //
+// user story is a guide of the user's journey though our application and serves as a guide for the developer in building out logic
+
 // When the user clicks begin, the timer should start and the squares should populate with a random color
 
 // When the user clicks on a color: the color should disapear and score should be added or subtracted.
