5c2631504ffccf14b4d4a0b3d92b0f965c47ce45
Gingaling <nycgingsf@hotmail.com>
2022-03-15 20:32:17 -0700

    Committing files.

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -8,3 +8,86 @@ console.log("Welcome to Poke-A-Square...");
 // When the user clicks on a color: the color should disapear and score should be added or subtracted.
 
 // When the round is over, the scores round and timer should be updated for the user to start over with increased difficulty.
+
+let score = 0;
+let time = 0;
+let round = 1;
+
+const updateTimer = () => {
+    time--;
+    $('#timer').text(`timer: ${time}`);
+    if (time === 0) {
+        time = 30;
+        round += 1;
+        setUpRound;
+    }
+};
+}
+
+setInterval(updateTimer, 1000)
+
+const setUpRound = () => {
+    $('.squares).empty();
+    $('#round').text('round: ' + round)
+
+    if (round === 1) {
+        createSquares(50);
+    } else if (round === 2) {
+        createSquares(100);
+    } else if (round >= 3) {
+        createSquares(150);
+    }
+}
+}
+
+
+
+
+const createSquares = (numberOfSquares) => {
+    for (i = 0; i < numberOfSquares; i++) {
+        const square = $('<div/>');
+        applyRandomColor(square);
+        square.on('click', handlePoke);
+        $('.squares').append(square);
+    createSquares(30);
+}
+}
+
+const applyRandomColor  = (square) => {
+    const randNum = Math.ceil(Math.random() * 3);
+    if(randNum === 1) {
+        square.css('background=color', 'red')
+    } else if(randNum === 2) {
+        square.css('background=color', 'blue')
+    } else if(randNum === 3) {
+        square.css('background=color', 'green')
+    }
+}
+
+const handlepoke = (e) => {
+    console.log(e.target)
+    $(e.target).css('opacity',0);
+    const color = $(e.target).css('background-color');
+    checkValidPoke(color)
+};
+
+console.log(color)
+
+const checkValidPoke = (square) => {
+    console.log(square, typeof square)
+    const colors = square.substring(4, square.length-1.split(", ");
+    const blue=coloros[2];
+    
+    if (blue ==='255'){
+        score++;
+    } else {
+        scpre--;
+    }
+    $('h1').text('scoreboard: ' + score);
+}
+}
+
+const updateTimer = () => {
+    time--;
+    $('#timer').text(`timer: ${time}s`);')
+}
