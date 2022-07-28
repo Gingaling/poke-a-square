800c1a384cc4c7edfd1e58529631714c2a6e7615
Gingaling <nycgingsf@hotmail.com>
2022-07-27 20:04:08 -0700

    Committing files - added some animation and made other design changes

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,4 +1,4 @@
-console.log("Welcome to Poke-A-Square...");
+console.log('Welcome to Poke-A-Square...');
 
 // == User Stories/game logic == //
 // user story is a guide of the user's journey though our application and serves as a guide for the developer in building out logic
@@ -9,85 +9,91 @@ console.log("Welcome to Poke-A-Square...");
 
 // When the round is over, the scores round and timer should be updated for the user to start over with increased difficulty.
 
-let score = 0;
-let time = 0;
+let time = 30;
 let round = 1;
 
-const updateTimer = () => {
-    time--;
-    $('#timer').text(`timer: ${time}`);
-    if (time === 0) {
-        time = 30;
-        round += 1;
-        setUpRound;
-    }
-};
+const setTimer = () => {
+	const timer = setInterval(() => {
+		time--;
+		if (time === 0) {
+			round++;
+			clearInterval(timer);
+			setUpRound();
+			setTimer();
+		}
+		updateTimer();
+	}, 1000);
 }
 
-setInterval(updateTimer, 1000)
+$('button').on('click', () => {
+	createSquares(30);
+	setTimer();
+	setUpRound();
+});
+
+const createSquares = numberOfSquares => {
+	const $squares = $('.squares');
+	for (let i = 0; i < numberOfSquares; i++) {
+		const $square = $('<div/>');
+		$square.addClass('square');
+		$square.css('background-color', applyRandomColor());
+		$squares.append($square);
+	}
+};
+
+const applyRandomColor = square => {
+	const colors = ['red', 'green', 'blue'];
+	const idx = Math.ceil(Math.random() * 3);
+	return colors[idx];
+};
+
+
+$(".squares").on("click", ".square", e => {
+	handlePoke(e);
+});
+
+let score = 0;
+
+const handlePoke = e => {
+	if (e.target.classList.contains("poked")) {
+		return;
+	}
+	if (e.target.classList.contains("square")) {
+		$(e.target).css('opacity', 0);
+		const color = $(e.target).css('background-color');
+		$(e.target).addClass("poked");
+		checkValidPoke(color);
+	}
+};
+
+const checkValidPoke = squareColor => {
+	const colors = squareColor.substring(4, squareColor.length - 1).split(" ");
+	const blue = parseInt(colors[2]);
+	if (blue === 255) {
+		score++;
+		$('h1').text('scoreboard: ' + score);
+	} else {
+		score--;
+		$('h1').text('scoreboard: ' + score);
+	}
+};
+
+const updateTimer = () => {
+	const $timer = $("#timer");
+	$timer.text(`timer: ${time}s`);
+};
 
 const setUpRound = () => {
-    $('.squares).empty();
-    $('#round').text('round: ' + round)
-
-    if (round === 1) {
-        createSquares(50);
-    } else if (round === 2) {
-        createSquares(100);
-    } else if (round >= 3) {
-        createSquares(150);
-    }
-}
-}
-
-
-
-
-const createSquares = (numberOfSquares) => {
-    for (i = 0; i < numberOfSquares; i++) {
-        const square = $('<div/>');
-        applyRandomColor(square);
-        square.on('click', handlePoke);
-        $('.squares').append(square);
-    createSquares(30);
-}
-}
-
-const applyRandomColor  = (square) => {
-    const randNum = Math.ceil(Math.random() * 3);
-    if(randNum === 1) {
-        square.css('background=color', 'red')
-    } else if(randNum === 2) {
-        square.css('background=color', 'blue')
-    } else if(randNum === 3) {
-        square.css('background=color', 'green')
-    }
-}
-
-const handlepoke = (e) => {
-    console.log(e.target)
-    $(e.target).css('opacity',0);
-    const color = $(e.target).css('background-color');
-    checkValidPoke(color)
+	$('.squares').empty();
+	$('#round').text('round: ' + round);
+	if (round === 1) {
+		createSquares(50);
+		time = 30;
+	} else if (round === 2) {
+		createSquares(100);
+		time = 20;
+	} else if (round >= 3) {
+		createSquares(150);
+		time = 10;
+	}
 };
-
-console.log(color)
-
-const checkValidPoke = (square) => {
-    console.log(square, typeof square)
-    const colors = square.substring(4, square.length-1.split(", ");
-    const blue=coloros[2];
-    
-    if (blue ==='255'){
-        score++;
-    } else {
-        scpre--;
-    }
-    $('h1').text('scoreboard: ' + score);
-}
-}
-
-const updateTimer = () => {
-    time--;
-    $('#timer').text(`timer: ${time}s`);')
-}
